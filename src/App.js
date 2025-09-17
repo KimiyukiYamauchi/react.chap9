151aba659651fa77c29d2e146e36caf7af1eae3d
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2025-09-17 09:53:03 +0900

    完成

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import { useState } from "react";
-import axios from "axios";
+import { useFetchUsers } from "./hooks/useFetchUsers";
 
 export const App = () => {
-  const [useList, setUseList] = useState([]);
-  const [isLooding, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-
-  const onClickFetchUsers = () => {
-    setIsLoading(true);
-    setIsError(false);
-    axios
-      .get("/users.json")
-      .then((result) => {
-        const users = result.data.map((user) => ({
-          id: user.id,
-          name: `${user.lastname} ${user.firstname}`,
-          age: user.age,
-        }));
-        setUseList(users);
-      })
-      .catch(() => setIsError(true))
-      .finally(() => setIsLoading(false));
-  };
+  // カスタムフックの利用
+  // 関数を実行し返却値を分割代入で受け取る
+  const { userList, isLooding, isError, onClickFetchUsers } = useFetchUsers();
 
   return (
     <div>
@@ -30,7 +13,7 @@ export const App = () => {
       {isLooding ? (
         <p>データ取得中です</p>
       ) : (
-        useList.map((user) => (
+        userList.map((user) => (
           <p key={user.id}>{`${user.id}: ${user.name} (${user.age}歳)`}</p>
         ))
       )}
--- src/hooks/useFetchUsers.js
+++ src/hooks/useFetchUsers.js
@@ -1,9 +1,34 @@
 import { useState } from "react";
+import axios from "axios";
 
 export const useFetchUsers = () => {
-  const [userList, setUserList] = useState([{ id: 1 }]);
+  const [userList, setUseList] = useState([]);
+  const [isLooding, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  const onClickFetchUsers = () => alert("関数実行");
+  // ユーザ取得ボタン押下アクション
+  const onClickFetchUsers = () => {
+    // ボタン押下時にローディングフラグをon、エラーフラグをoffにする
+    setIsLoading(true);
+    setIsError(false);
+    axios
+      .get("/users.json")
+      .then((result) => {
+        // 苗字と名前を結合するように変換
+        const users = result.data.map((user) => ({
+          id: user.id,
+          name: `${user.lastname} ${user.firstname}`,
+          age: user.age,
+        }));
+        // ユーザー一覧stateを更新
+        setUseList(users);
+      })
+      // エラーが発生した場合はエラーフラグをonにする
+      .catch(() => setIsError(true))
+      // 通信完了後にローディングフラグをoffにする
+      .finally(() => setIsLoading(false));
+  };
 
-  return { userList, onClickFetchUsers };
+  // まとめて返却したいのでオブジェクトを設定する
+  return { userList, isLooding, isError, onClickFetchUsers };
 };
